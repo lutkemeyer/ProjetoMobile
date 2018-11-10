fb4034200614f7fd93fc9f4a6c224ad0790c9bf4
lutkemeyer <guilherme_eyng@hotmail.com>
2018-11-10 00:26:06 -0200

    Criado enum de temas para facil controle

--- app/src/main/java/com/example/guilhermeeyng/projetomobile/utilitarios/Tema.java
+++ app/src/main/java/com/example/guilhermeeyng/projetomobile/utilitarios/Tema.java
@@ -1,4 +1,15 @@
 package com.example.guilhermeeyng.projetomobile.utilitarios;
 
 public enum Tema {
+
+    PADRAO(1),
+    CLARO(2),
+    ESCURO(3),
+    CUSTOMIZADO(4);
+
+    private int i;
+
+    Tema(int i){
+        this.i = i;
+    }
 }
